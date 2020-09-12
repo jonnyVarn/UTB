7b8c98f20fa3567be23a3d8de9b10912fce27bb8
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2020-09-12 18:06:02 +0200

    Create Calc.java

---
+++ Calc.java
@@ -0,0 +1,43 @@
+public class Calc { 
+    public static void main(Double[] args ) {
+    
+
+
+      
+    } // main 
+   
+    //-------plus---------------------------------------------------
+
+     public static double plus (double num1, double num2) {
+        double sum = num1 + num2;
+        return sum;
+    }
+
+
+    //-------minus---------------------------------------------------
+
+    public static double minus (double num1, double num2) {
+      double sum = num1 - num2;
+      return sum;
+  }
+
+  //--------times------------------------------------------------------
+
+  public static double times(double num1, double num2) {
+      double sum = num2 * num2;
+      return sum;
+  }
+  //--------division----------------------------------------------------
+
+  public static double division(double num1, double num2) {
+      double sum = num2 / num2;
+      return sum;
+  }
+  //-------------------------------------------------------------------
+
+     
+}
+
+
+   //calc
+         
